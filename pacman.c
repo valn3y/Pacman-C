7910dbb47bcb49d2db14216be730a10d58845166
Valney Marinho do Nascimento Júnior <valney@motorola.com>
2022-10-03 11:49:37 -0400

    create actions

--- pacman.c
+++ pacman.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "pacman.h"
 
-char** map;
-int lines;
-int columns;
+MAPA m;
 
 void allocMemoryMap() {
-	map = malloc(sizeof(char*) * lines);
-	for(int i = 0; i < lines; i++) {
-		map[i] = malloc(sizeof(char*) * (columns + 1));
+	m.matrix = malloc(sizeof(char*) * m.lines);
+	for(int i = 0; i < m.lines; i++) {
+		m.matrix[i] = malloc(sizeof(char*) * (m.columns + 1));
 	}
 }
 
 void freeMemoryMap() {
-	for(int i = 0; i < lines; i++) {
-		free(map[i]);
+	for(int i = 0; i < m.lines; i++) {
+		free(m.matrix[i]);
 	}
-	free(map);
+	free(m.matrix);
 }
 
 void readMap() {
@@ -27,23 +26,69 @@ void readMap() {
 		exit(1);
 	}
 
-	fscanf(f, "%d %d", &lines, &columns);
+	fscanf(f, "%d %d", &(m.lines), &(m.columns));
 
 	allocMemoryMap();
 
 	for(int i = 0; i < 5; i++) {
 		// FULL LINE
-		fscanf(f, "%s", map[i]);
+		fscanf(f, "%s", m.matrix[i]);
 	}
 	fclose(f);
+}
+
+int finishGame() {
+	return 0;
+}
+
+void move(char direction) {
+	int x;
+	int y;
+
+	for(int i = 0; i < m.lines; i++) {
+		for(int j = 0; j < m.columns; j++) {
+			if(m.matrix[i][j] == '@') {
+				x = i;
+				y = j;
+				break;
+			}
+		}
+	}
+
+	switch(direction) {
+		case 'a':
+			m.matrix[x][y-1] = '@';
+			break;
+		case 'w':
+			m.matrix[x-1][y] = '@';
+			break;
+		case 's':
+			m.matrix[x+1][y] = '@';
+			break;
+		case 'd':
+			m.matrix[x][y+1] = '@';
+			break;
+	}
+
+	m.matrix[x][y] = '.';
+}
+
+void printMap() {
+	for(int i = 0; i < m.lines; i++) {
+		printf("%s\n", m.matrix[i]);
+	}
 }
 
 int main() {	
 	readMap();
 
-	for(int i = 0; i < 5; i++) {
-		printf("%s\n", map[i]);
-	}
+	do {
+		printMap();
+
+		char command;
+		scanf(" %c", &command);
+		move(command);
+	} while(!finishGame());
 
 	freeMemoryMap();
 }
--- pacman.h
+++ pacman.h
@@ -1,3 +1,14 @@
 void freeMemoryMap();
 void readMap();
 void allocMemoryMap();
+int finishGame();
+void move(char direction);
+void printMap();
+
+struct map {
+	char** matrix;
+	int lines;
+	int columns;
+};
+
+typedef struct map MAPA;
